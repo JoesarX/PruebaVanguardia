d4dc31fb7e1b57ff0cb0fc34449fa65bc35b4cef
Josue <josueespinalsanz@gmail.com>
2023-10-19 19:54:34 -0600

    initial expressjs commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.get('/josue/', (req, res) => {
+  res.send('Josue El Crack')
+})
+
+app.get('/vanguardia/', (req, res) => {
+  res.send('Estamos en clase de Tecnologias de Vanguardia')
+})
+
+app.listen(3000)
