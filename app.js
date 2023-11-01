acb47bb523902b9c26b90ee771de4d376e0f81d5
Josue <josueespinalsanz@gmail.com>
2023-10-31 19:50:39 -0600

    alumnos old

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-
-const app = express();
-const port = 3000;
-
-const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'test';
-
-const client = new MongoClient(url);
+const express = require('express')
+const app = express()
+const port = 3000
+const studentRouter=require("./Routes/StudentRoutes")
+var mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.json());
 
-app.post('/insertarMongo/', async (req, res) => {
-  try {
-    await client.connect();
-    const db = client.db(dbName);
+app.use("/students",studentRouter);
 
-    const document = {
-      nombre: req.body.nombre,
-      edad: req.body.edad,
-      curso: req.body.curso,
-      // nombre: 'Juanito Pruebas',
-      // edad: 21,
-      // curso: 'VanGuardia',
-    };
-
-    const result = await db.collection('test').insertOne(document);
-
-    res.status(201).json({ message: 'Documento insertado', insertedId: result.insertedId });
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Ocurrió un error' });
-  } finally {
-    await client.close();
-  }
+mongoose.connect(process.env.MOONGOSEKEY, {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log("MongoDB connected successfully");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
 });
+
+
+// app.post('/insertarMongo/', async (req, res) => {
+//   try {
+//     await client.connect();
+//     const db = client.db(dbName);
+
+//     const document = {
+//       nombre: req.body.nombre,
+//       edad: req.body.edad,
+//       curso: req.body.curso,
+//       // nombre: 'Juanito Pruebas',
+//       // edad: 21,
+//       // curso: 'VanGuardia',
+//     };
+
+//     const result = await db.collection('test').insertOne(document);
+
+//     res.status(201).json({ message: 'Documento insertado', insertedId: result.insertedId });
+//   } catch (err) {
+//     console.error('Error:', err);
+//     res.status(500).json({ error: 'Ocurrió un error' });
+//   } finally {
+//     await client.close();
+//   }
+// });
 
 app.get('/', (req, res) => {
   res.send('hello world');
